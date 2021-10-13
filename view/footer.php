4f20675671e9ec8b86db452c281df334f7e9f57a
Lucas Tadyszak <lucas.tadyszak@gmail.com>
2021-10-13 21:37:27 +0200

    Mise en forme de l'application

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,11 @@
         <footer>
 
         </footer>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.12/d3.min.js"></script>
+        <script src="https://cdpn.io/cp/internal/boomboom/pen.js?key=pen.js-e3b3964e-19e3-8091-df55-bbcd3d316f70" crossorigin=""></script>
+        <script src="https://cpwebassets.codepen.io/assets/editor/iframe/iframeConsoleRunner-d8236034cc3508e70b0763f2575a8bb5850f9aea541206ce56704c013047d712.js"></script>
+        <script src="https://cpwebassets.codepen.io/assets/editor/iframe/iframeRefreshCSS-4793b73c6332f7f14a9b6bba5d5e62748e9d1bd0b5c52d7af6376f3d1c625d7e.js"></script>
+        <script src="https://cpwebassets.codepen.io/assets/editor/iframe/iframeRuntimeErrors-4f205f2c14e769b448bcf477de2938c681660d5038bc464e3700256713ebe261.js"></script>
+        <script src="resources/script/script.js"></script>
     </body>
 </html>
--- view/navbar.php
+++ view/navbar.php
@@ -2,11 +2,14 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="theme-color" content="#1E384C">
         <title><?php if(!isset($y)){ echo $pageTitle;} ?></title>
-        <link rel="stylesheet" href=""> <!-- Inclure ici votre css -->
-        <script src=""></script> <!-- Inclure ici votre js -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+        <link rel="stylesheet" href="resources/css/style.css">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> <!-- Inclure ici votre favicon -->
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        
+        
     </head>
     <body>
